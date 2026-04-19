5b2e86823eafdff690cf94d5ceca45fa4d9333af
Victor Carrasco <vc.business.oo@gmail.com>
2026-04-19 13:04:16 +0200

    Se ha añadido logica para la gestion de descuentos basandose en si es o no año bisiesto

--- funciones.py
+++ funciones.py
@@ -7,12 +7,19 @@ def saber_anyo_bisiesto(anyo:int) -> bool:
     return bisiesto
     
 
-print(saber_anyo_bisiesto(2000))
-print(saber_anyo_bisiesto(2400))
-print(saber_anyo_bisiesto(1900))
-
-
 def calculo_precio_total(precio_absoluto:float, descuento:int) -> float:
     return precio_absoluto - precio_absoluto * descuento / 100
 
-print(calculo_precio_total(100, 15))
+
+years = [2000, 1980, 2024, 2025]
+product_price = 1052.34
+for year in years:
+    final_price = 0
+    if saber_anyo_bisiesto(year) == True:
+        final_price = calculo_precio_total(product_price, 22)
+    else:
+        final_price = calculo_precio_total(product_price, 10)
+    
+    final_price = round(final_price, 2)
+    
+    print(f"El precio de la TV en el año {year} es de {final_price}€")
